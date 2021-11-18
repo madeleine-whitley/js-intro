e8a2279b8889df7ff857467f05c04507b48375e6
Madeleine Whitley <maddy.whitley@gmail.com>
2021-11-17 19:33:22 -0700

    added nots on var and tranfer

---
+++ notes.js
@@ -0,0 +1,39 @@
+// Datatypes
+//   -string "" ''
+//   -boolean, true or fale 
+//   -integers 123124 -321313
+//   -float 23.45
+//   -hash {first_name: "bob", age: 2}
+//   -array ["" , 3213123, []]
+//   -Nil 
+//   -nan 
+//   -undefined 
+// + = /* %
+// ()
+// || &&
+// < > <= >=
+// =
+// ==
+// ===
+// !=
+// !==
+// ++
+// --
+// +=
+// -=
+// *=
+
+// variables
+// var first_name = 'bob'
+// = letters, numbers, _ $
+// - has to begin with a letter, $, or _ 
+// - cannot start off with a number
+// - case sensitive (first_name vs First_name - two different variables)
+// - stay away from reserve words, be descriptive 
+// -camelcase 
+// no longer using var because of scope 
+// var file wide scope 
+// let - things you want to change 
+// const -constant or things you don't want to change 
+// ^^ use these instead 
+// scope is wherever you define it 
